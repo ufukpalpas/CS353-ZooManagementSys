557a0e88e7d7d42d24020aefae8a3eeab02c9023
Ufuk Palpas <ufukpalpas@gmail.com>
2021-05-11 00:41:06 +0300

    keeper cage relation is handled
    reg food need to be implemented

--- src/animalsincage.php
+++ src/animalsincage.php
@@ -3,6 +3,8 @@ include("config.php");
 session_start();
 if($_SESSION['login_user']) // değişecek
 {
+    $cageid = $_SESSION['cageid'];
+
     $userid = $_SESSION['login_user']; // değişecek
     $namequery = "select name from user where user_id ='". $userid ."'"; //session name e kadarlık kısım indexin.html e alınacak buraya sessiondan çekme gelecek
     $namearr = mysqli_query($mysqli, $namequery);
@@ -83,62 +85,57 @@ if(isset($_POST['logout'])){
                 }
             </script>
             <section class="mainsec">
-                <h2>My Cages</h2>
-				<div style="width:75%; height:75%;  background-color:white; margin-left: 12.5%; margin-top: 20px; border-radius: 20px;">
-            	<hr style="margin-left: 20px; margin-right: 20px;">
-				<?php
-				$query = 
+                <?php
+                echo "<h2>Animals in Cage #$cageid</h2>
+				<div style=\"width:87%; height:87%; background-color:white; margin-left: 12.5%; margin-top: 20px; border-radius: 20px;\">
+            	<hr style=\"margin-left: 20px; margin-right: 20px;\">";
+				
+				/*$query = 
 				"select *
 				 from cage c, assigned a
-				 where a.keep_id = \"". $userid ."\" and c.cage_id = a.cage_id;";
+				 where a.keep_id = \"". $userid ."\" and c.cage_id = a.cage_id;";*/
 				echo "<table style=\"width:90%; margin-top: 10px; margin-left: 60px; margin-right: 10px;\">";
 				echo "<tr class=\"toptable\">
-						<th class=\"thtitle\">Cage ID<hr></th>    
-						<th class=\"thtitle\">Cage Type<hr></th>    
-						<th class=\"thtitle\">Animal Count<hr></th>    
-						<th class=\"thtitle\">Last Care Date<hr></th>   
-						<th class=\"thtitle\">Feed Time<hr></th>  
-						<th class=\"thtitle\">Location<hr></th>  
-						<th class=\"thtitle\">Food<hr></th>   
-						<th class=\"thtitle\">Select for any operation<hr></th>   
+						<th class=\"thtitle\">Animal ID<hr></th>    
+						<th class=\"thtitle\">Name<hr></th>    
+						<th class=\"thtitle\">Species<hr></th>    
+						<th class=\"thtitle\">Birth Date<hr></th>   
+						<th class=\"thtitle\">Gender<hr></th>  
+						<th class=\"thtitle\">Weight<hr></th>  
+						<th class=\"thtitle\">Height<hr></th>   
+                        <th class=\"thtitle\">Origin<hr></th> 
+                        <th class=\"thtitle\">Endangered<hr></th> 
+                        <th class=\"thtitle\">Habitat<hr></th> 
+                        <th class=\"thtitle\">Diet<hr></th> 
+                        <th class=\"thtitle\">Status<hr></th> 
+                        <th class=\"thtitle\">Last Health Check<hr></th> 
+                        <th class=\"thtitle\">Treatment<hr></th> 
+						<th class=\"thtitle\">Training<hr></th>  
 					</tr>";
-				$arr = array();
-				if($result = $mysqli->query($query)){
-					$i = 0;
+				/*if($result = $mysqli->query($query)){
 					while(($row = $result->fetch_assoc())!= null)  {
 						echo "<tr>
-								<th>". $row['cage_id'] ."<hr></th>    
-								<th>". $row['cage_type'] ."<hr></th>    
-								<th>". $row['animal_count'] ."<hr></th>    
-								<th>". $row['last_care_date'] ."<hr></th>    
-								<th>". $row['feed_time'] ."<hr></th>    
-								<th>". $row['location'] ."<hr></th> 
-								<th><form method=\"post\"><input type=\"submit\" name=\"".$i."reg\"class=\"btn3\" value=\"Regularize Food\"></form><hr></th>  
-								<th><form method=\"post\"><input type=\"submit\" name=\"".$i."select\" class=\"btn3\" value=\"Select\"></form><hr></th>  
+								<th>". $row['animal_id'] ."<hr></th>    
+								<th>". $row['name'] ."<hr></th>    
+								<th>". $row['species'] ."<hr></th>    
+								<th>". $row['date_of_birth'] ."<hr></th>    
+								<th>". $row['gender'] ."<hr></th>    
+								<th>". $row['weight'] ."<hr></th> 
+                                <th>". $row['height'] ."<hr></th> 
+                                <th>". $row['origin'] ."<hr></th> 
+                                <th>". $row['isEndangered'] ."<hr></th> 
+                                <th>". $row['habitat'] ."<hr></th> 
+                                <th>". $row['diet'] ."<hr></th> 
+                                <th>". $row['status'] ."<hr></th> 
+                                <th>". $row['last_health_check'] ."<hr></th> 
+                                <th><form method=\"post\"><input type=\"submit\" class=\"btn3\" value=\"Request Treatment\"></form><hr></th>  
+								<th><form method=\"post\"><input type=\"submit\" class=\"btn3\" value=\"Training Information\"></form><hr></th>  
 							</tr>";
-						$arr[$i] = $row['cage_id'];
-						$i = $i + 1;
 					}
 				} else {
-					echo "Error while retrieving cage table. Error: " . mysqli_error($mysqli);
-				}
+					echo "Error while retrieving animal table. Error: " . mysqli_error($mysqli);
+				}*/
 				echo "</table>";
-
-				if($_SERVER["REQUEST_METHOD"] == "POST"){
-					for($k = 0; $k < count($arr); $k++){
-						$place = strval($k) . "select";
-						$place2 = strval($k) . "reg";
-						if(isset($_POST[$place])){
-							$_SESSION['cageid'] = $arr[$k];
-							header("location: animalsincage.php");
-							break;
-						}
-						if(isset($_POST[$place2])){
-							// REGULARIZE FOOD ----------------------------------------------------------
-							break;
-						}
-					}
-				}
 				?>
 				</div>
 			</section>
