94321d1cd7474dda6a5445b9e32e6b48bca7102a
Ufuk Palpas <ufukpalpas@gmail.com>
2021-05-11 15:11:11 +0300

    Group tours are added

--- src/grouptourin.php
+++ src/grouptourin.php
@@ -7,6 +7,11 @@ if($_SESSION['login_user'])
     $name = $_SESSION['name'];
     if($_SESSION['type'] == "visitor"){
         $money = $_SESSION['money'];
+		$typequery = "select discount_type from visitor where user_id ='". $userid ."'"; 
+        $typearr = mysqli_query($mysqli, $typequery);
+        $fetcharr = mysqli_fetch_array($typearr, MYSQLI_ASSOC);
+        $type = $fetcharr['discount_type'];
+
     }
 } else {
     header("location: grouptour.php");
@@ -88,12 +93,153 @@ if(isset($_POST['logout'])){
                     <p>
                         You can join our guided group tours. You can enjoy and learn by paying reasonable prices. <br>
                         Learn about their life... <br>
-                        To book your place in the group tour Sign Up or Sign In now....
                     </p>
                 </article>
+				
+				<h2>Current Group Tours</h2>
+				<div style="width:75%; height:fit-content;  background-color:white; margin-left: 12.5%; border-radius: 20px; ">
+            	<hr style="margin-left: 20px; margin-right: 20px;">
+				<?php
+				$query = 
+				"select g.event_id, e.start_date, g.capacity, g.participant_count, e.duration, e.user_id
+				 from group_tour g, event e
+                 where e.event_id = g.event_id and g.event_id not in (select event_id from pay where user_id = $userid);";
+
+				echo "<table style=\"width:90%; margin-top: 10px; margin-left: 60px; margin-right: 10px;\">";
+				echo "<tr class=\"toptable\">
+						<th class=\"thtitle\">No<hr></th>    
+						<th class=\"thtitle\">Duration<hr></th>    
+						<th class=\"thtitle\">Start Date<hr></th>    
+						<th class=\"thtitle\">Capacity<hr></th>   
+                        <th class=\"thtitle\">Participant Count<hr></th>   
+						<th class=\"thtitle\">Amount<hr></th>   
+						<th class=\"thtitle\">Join<hr></th>   
+					</tr>";
+				$arr = array();
+				$arr2 = array();
+				if($result = $mysqli->query($query)){
+					$i = 0;
+					while(($row = $result->fetch_assoc())!= null)  {
+						echo "<form method=\"post\"><tr>
+								<th>". $row['event_id'] ."<hr></th>    
+								<th>". $row['duration'] ."<hr></th>    
+								<th>". $row['start_date'] ."<hr></th>    
+								<th>". $row['capacity'] ."<hr></th>    
+								<th>". $row['participant_count'] ."<hr></th>";   
+						if($type == 1){
+							echo "<th>$5<hr></th>";
+						} else {
+							echo "<th>$10<hr></th>";
+						}
+						echo	"<th><form method=\"post\"><input type=\"submit\" name=\"".$i."\" class=\"btn3\" value=\"Join\"></form><hr></th>  
+							</tr></form>";
+						$arr[$i] = $row['event_id'];
+						$arr2[$i] = $row['user_id'];
+						$i = $i + 1;
+					}
+				} else {
+					echo "Error while retrieving group_tour table. Error: " . mysqli_error($mysqli);
+				}
+				echo "</table>";
+
+				if($_SERVER["REQUEST_METHOD"] == "POST"){
+					for($k = 0; $k < count($arr); $k++){
+						if(isset($_POST[$k])){
+							$amount = 10;
+							if($type == 1)
+								$amount = 5;
+                            $payquery = "insert into pay(coor_id, event_id, user_id, requested_amount) values(\"".$arr2[$k]."\", \"".$arr[$k]."\", \"$userid\",\"$amount\");";
+                            $checkgtquery = "select participant_count, capacity from group_tour where event_id = ".$arr[$k];
+							$upgtquery = "update group_tour set participant_count = participant_count + 1 where event_id = ".$arr[$k];
+							$moneyquery = "update visitor set money = money - $amount where user_id = $userid;";
+                            if($amount <= $money){
+								if($cresult = $mysqli->query($checkgtquery)){
+									$fetcharr = mysqli_fetch_array($cresult, MYSQLI_ASSOC);
+									$gt = $fetcharr['participant_count'];
+									$capa = $fetcharr['capacity'];
+									if($gt == $capa){
+										echo '<script type="text/JavaScript">
+										window.alert("There is no enough Capacity!");
+										window.location = "makedonation.php";
+										</script>';
+									} else {
+										if($dresult = $mysqli->query($payquery)){
+											if($ddresult = $mysqli->query($upgtquery)){
+												$mysqli->query($moneyquery);
+												echo '<script type="text/JavaScript">
+												window.alert("Joined Successfully!");
+												window.location = "grouptourin.php";
+												</script>';
+											} else {
+												echo '<script type="text/JavaScript">
+												window.alert("Update Failed!'.mysqli_error($mysqli).'");
+												window.location = "grouptourin.php";
+												</script>';
+											}
+										} else {
+											echo '<script type="text/JavaScript">
+											window.alert("Pay Failed!'.mysqli_error($mysqli).'");
+											window.location = "grouptourin.php";
+											</script>';
+										}
+									}
+								} else {
+									echo '<script type="text/JavaScript">
+									window.alert("participant_count check failed!");
+									window.location = "grouptourin.php";
+									</script>';
+								}
+                            } else {
+                                echo '<script type="text/JavaScript">
+                                window.alert("You do not have enough money!");
+                                window.location = "grouptourin.php";
+                                </script>';
+                            }
+                            break;
+						}
+					}
+				}
+				?>
+				</div>
+
+				<h2>Joined Group Tours</h2>
+				<div style="width:75%; height:fit-content;  background-color:white; margin-left: 12.5%; margin-top: 20px; border-radius: 20px; margin-bottom:20%;">
+            	<hr style="margin-left: 20px; margin-right: 20px;">
+				<?php
+				$queryshow = 
+				"select p.event_id, e.duration, e.start_date, g.capacity, g.participant_count, p.requested_amount
+				 from pay p, group_tour g, event e
+                 where p.user_id = $userid and e.event_id = p.event_id and e.event_id = g.event_id;";
+
+				echo "<table style=\"width:90%; margin-top: 10px; margin-left: 60px; margin-right: 10px;\">";
+				echo "<tr class=\"toptable\">
+						<th class=\"thtitle\">No<hr></th>    
+						<th class=\"thtitle\">Duration<hr></th>    
+						<th class=\"thtitle\">Start Date<hr></th>    
+						<th class=\"thtitle\">Capacity<hr></th>   
+                        <th class=\"thtitle\">Participant Count<hr></th>   
+						<th class=\"thtitle\">Paid Amount<hr></th>  
+					</tr>";
+				if($result5 = $mysqli->query($queryshow)){
+					while(($row2 = $result5->fetch_assoc())!= null)  {
+						echo "<form method=\"post\"><tr>
+								<th>". $row2['event_id'] ."<hr></th>    
+								<th>". $row2['duration'] ."<hr></th>    
+								<th>". $row2['start_date'] ."<hr></th>    
+								<th>". $row2['capacity'] ."<hr></th>    
+                                <th>". $row2['participant_count'] ."<hr></th>     
+								<th>$". $row2['requested_amount'] ."<hr></th>   
+							</tr></form>";
+					}
+				} else {
+					echo "Error while retrieving pay table. Error: " . mysqli_error($mysqli);
+				}
+				echo "</table>";
+				?>
+				</div>
 			</section>
 		</main>
-		<div class="wrapper"> <!-- alt kısım -->
+		<div class="wrapper"> 
 			<footer>
 				<div class="cont">
 				<div class="add-link-div">
