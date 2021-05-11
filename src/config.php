4c082f2c7a67f409b4a9551c575c0bb24f3015c5
Asya Doğa Özer <49779152+AsyaDoga@users.noreply.github.com>
2021-05-11 21:54:06 +0300

    Update config.php
    own config

--- src/config.php
+++ src/config.php
@@ -1,8 +1,8 @@
 <?php
 if(!defined('host')) define('host', 'dijkstra.ug.bcc.bilkent.edu.tr');
-if(!defined('dbname')) define('dbname', 'ufuk_palpas');
-if(!defined('username')) define('username', 'ufuk.palpas');
-if(!defined('password')) define('password', 'RYFTwiPs');
+if(!defined('dbname')) define('dbname', 'asya_ozer');
+if(!defined('username')) define('username', 'asya.ozer');
+if(!defined('password')) define('password', 'YdsjunAB');
 $mysqli = mysqli_connect(host, username, password, dbname);
 if($mysqli === FALSE) {
     die("Connection Failed to MySQL. Error: " . mysqli_connect_error());
