071b3a3c4b5504f26109de25fb79b5bb9ced8bfc
Ufuk Palpas <ufukpalpas@gmail.com>
2021-05-17 01:22:24 +0300

    allcomments updated for all user types

--- src/allcomments.php
+++ src/allcomments.php
@@ -2,6 +2,179 @@
 include("config.php"); 
 session_start();
 
+function vislogin($mysqli){
+	$emaillgn = $_POST['emaillgn'];
+    $password = $_POST['password'];
+	$query = " select user_id, email, password from user where email=\"$emaillgn\" and password=\"$password\"";
+    if(($result = $mysqli->query($query))) {
+		if($result->num_rows == 1) {
+			$fetcharr0 = mysqli_fetch_array($result, MYSQLI_ASSOC);
+			$userid = $fetcharr0['user_id'];
+			$query1 = " select user_id from visitor where user_id=\"$userid\"";
+			if(($result1 = $mysqli->query($query1))){
+					if($result1->num_rows == 1){
+						$_SESSION['login_user'] = $userid;
+						$_SESSION['type'] = "visitor";
+		
+						$namequery = "select name from user where user_id ='". $userid ."'"; 
+						$namearr = mysqli_query($mysqli, $namequery);
+						$fetcharr = mysqli_fetch_array($namearr, MYSQLI_ASSOC);
+						$name = $fetcharr['name'];
+						$_SESSION['name'] = $name;
+		
+						$moneyquery = "select money from visitor where user_id ='". $userid ."'"; 
+						$moneyarr = mysqli_query($mysqli, $moneyquery);
+						$fetchmarr = mysqli_fetch_array($moneyarr, MYSQLI_ASSOC);
+						$money = $fetchmarr['money'];
+						$_SESSION['money'] = $money;
+						header("location: allcommentsin.php");
+					} else {
+						echo '<script type="text/JavaScript">
+						window.alert("Please use employee login!");
+						window.location = "allcomments.php";
+						</script>';
+					}
+			}else{
+				echo '<script type="text/JavaScript">
+				window.alert("Query2 operation failed!");
+				window.location = "allcomments.php";
+				</script>';
+			}
+		} else {
+			echo '<script type="text/JavaScript">
+					window.alert("Incorrect email or password please try again!");
+					window.location = "allcomments.php";
+					</script>';
+		}
+    } else {
+        echo '<script type="text/JavaScript">
+                window.alert("Query operation failed!");
+                window.location = "allcomments.php";
+            </script>';
+    }
+}
+function register($mysqli){
+	$fullname = $_POST['fullname'];
+	$email = $_POST["email"];
+	$phoneNum = $_POST["phoneno"];
+	$date = $_POST["dateofbirth"];
+    $password = $_POST['password'];
+	$confpassword = $_POST['confpass'];
+	$gender = $_POST['gender'];
+	$account_type = $_POST['type'];
+
+	if($confpassword != $password){
+		echo '<script type="text/JavaScript">
+		window.alert("Your Passwords do not Match!");
+		window.location = "allcomments.php";
+		</script>';
+		return;
+	}
+	$query = "insert into user(name, phone_number, email, gender, date_of_birth, password)"
+		."values(\"" .$fullname. "\", \"" .$phoneNum. "\", \"" .$email. "\", \"" .$gender. "\", \"" . $date ."\", \"" .$password. "\");";
+		if($result = $mysqli->query($query)) {
+			$account = 0;
+			if($account_type == "Student")
+				$account = 1;
+			$last_id = $mysqli->insert_id;
+			$query2 = "insert into visitor(user_id, discount_type) values(\"".$last_id."\", \"".$account."\");";
+			if($result3 = $mysqli->query($query2)){
+				echo '<script type="text/JavaScript">
+				window.alert("Register Successful!");
+				window.location = "allcomments.php";
+				</script>';
+			}
+    } else {
+        echo '<script type="text/JavaScript">
+                window.alert("Query operation failed!");
+                window.location = "allcomments.php";
+            </script>';
+    }
+}
+function emplogin($mysqli){
+	$emailemp = $_POST['emailemp'];
+    $password = $_POST['emppass'];
+	$query = " select user_id, email, password from user where email=\"$emailemp\" and password=\"$password\"";
+    if($result = $mysqli->query($query)) {
+		if($result->num_rows == 1){
+			$fetcharr0 = mysqli_fetch_array($result, MYSQLI_ASSOC);
+			$userid = $fetcharr0['user_id'];
+
+			$query1 = " select user_id from keeper where user_id=\"$userid\"";
+			$query2 = " select user_id from veterinarian where user_id=\"$userid\"";
+			$query3 = " select user_id from coordinator where user_id=\"$userid\"";
+			$query4 = " select user_id from guide where user_id=\"$userid\"";
+
+			$result1 = $result = $mysqli->query($query1);
+			$result2 = $result = $mysqli->query($query2);
+			$result3 = $result = $mysqli->query($query3);
+			$result4 = $result = $mysqli->query($query4);
+
+			if($result1->num_rows == 1){ //keeper
+				$_SESSION['login_user'] = $userid;
+				$_SESSION['type'] = "keeper";
+				$namequery = "select name from user where user_id ='". $userid ."'"; 
+				$namearr = mysqli_query($mysqli, $namequery);
+				$fetcharr = mysqli_fetch_array($namearr, MYSQLI_ASSOC);
+				$name = $fetcharr['name'];
+				$_SESSION['name'] = $name;
+				header("location: allcommentsin.php");
+			} else if($result2->num_rows == 1){ //vet
+				$_SESSION['login_user'] = $userid;
+				$_SESSION['type'] = "vet";
+				$namequery = "select name from user where user_id ='". $userid ."'"; 
+				$namearr = mysqli_query($mysqli, $namequery);
+				$fetcharr = mysqli_fetch_array($namearr, MYSQLI_ASSOC);
+				$name = $fetcharr['name'];
+				$_SESSION['name'] = $name;
+				header("location: allcommentsin.php");
+			} else if($result3->num_rows == 1){ //coordinator
+				$_SESSION['login_user'] = $userid;
+				$_SESSION['type'] = "coor";
+				$namequery = "select name from user where user_id ='". $userid ."'"; 
+				$namearr = mysqli_query($mysqli, $namequery);
+				$fetcharr = mysqli_fetch_array($namearr, MYSQLI_ASSOC);
+				$name = $fetcharr['name'];
+				$_SESSION['name'] = $name;
+				header("location: allcommentsin.php");
+			} else if($result4->num_rows == 1){ //guide
+				$_SESSION['login_user'] = $userid;
+				$_SESSION['type'] = "guide";
+				$namequery = "select name from user where user_id ='". $userid ."'"; 
+				$namearr = mysqli_query($mysqli, $namequery);
+				$fetcharr = mysqli_fetch_array($namearr, MYSQLI_ASSOC);
+				$name = $fetcharr['name'];
+				$_SESSION['name'] = $name;
+				header("location: allcommentsin.php");
+			} else {
+				echo '<script type="text/JavaScript">
+				window.alert("Please use visitor login!");
+				window.location = "allcomments.php";
+				</script>';
+			}
+		} else {
+			echo '<script type="text/JavaScript">
+					window.alert("Incorrect email or password please try again!");
+					window.location = "allcomments.php";
+			</script>';
+		}
+    } else {
+        echo '<script type="text/JavaScript">
+                window.alert("Query operation failed!");
+                window.location = "allcomments.php";
+            </script>';
+    }
+}
+if(isset($_POST['vislogin'])){
+    vislogin($mysqli);
+}
+if(isset($_POST['register'])){
+	register($mysqli);
+}
+if(isset($_POST['emploginn'])){
+	emplogin($mysqli);
+}
+
 function console_log( $data ){
     echo '<script>';
     echo 'console.log('. json_encode( $data ) .')';
@@ -16,6 +189,8 @@ function console_log( $data ){
         .title{
             font: 40px helvetica;
             margin-bottom: 10px;
+            border-bottom: 3px solid #d5d5d5;
+            margin-top: 5%;
         }
         .table{
             border-collapse: collapse;
@@ -46,61 +221,112 @@ function console_log( $data ){
 
 <title> KasaloZoo </title>
 <meta charset = "UTF-8">
-<link rel="stylesheet" href="loginstyle.css">
+<link rel="stylesheet" href="style.css">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="owlcarousel/assets/owl.carousel.min.css">
-<link rel="stylesheet" href="owlcarousel/assets/owl.theme.default.min.css">
 </head>
 
 <body>
-<header>
-<a href="indexin.php" class="header-brand">KasaloZoo</a>
-    <img class="logo" src="image/balina.png" alt="kasalot logo">
-    <nav>
-        <ul>
-            <li><a href="indexin.php">Main Page</a></li>
-            <li><a href="animalsin.php">Animals</a></li>
-            <li><a href="eventsin.php">Events</a></li>
-            <li><a href="aboutin.php">About Zoo</a></li>
-            <?php
-            echo "<li>
-                <a href=\"#\" onclick=\"toggleuserPopup()\">Hello $name ($userid)
-                <img class=\"down\" src=\"image/user.png\" alt=\"user logo\">
-                </a>
-            </li>";
-            if($usertype == "visitor")
-                echo "<li><a href=\"#\">$money
-                <img class=\"down\" src=\"image/dollar.png\" alt=\"dollar logo\">
-                </a></li>";
-            ?>
-        </ul>
-    </nav>
-</header>
+        <header>
+			<a href="index.php" class="header-brand">KasaloZoo</a>
+			<img class="logo" src="image/balina.png" alt="kasalot logo">
+			<nav>
+				<ul>
+					<li><a href="index.php">Main Page</a></li>
+					<li><a href="animals.php">Animals</a></li>
+					<li><a href="events.php">Events</a></li>
+					<li><a href="about.php">About Zoo</a></li>
+					<li><a href="#" onclick="togglePopup()">Sign In</a></li>
+					<li><a href="#" onclick="toggleRegPopup()">Sign Up</a></li>
+				</ul>
+			</nav>
+		</header>
 <main>
-<div class="user-popup" id="user-popup">
-        <div class="overlay"></div>
-        <div class="content">
-            <div class="close" onclick="toggleuserPopup()">×</div>
-            <h2 class="h2pop">Operations</h2>
-            <form method = "post">
-                <button name="viewp" class="btn">View Profile</a></button>
-                <button name="editp" class="btn">Edit Profile</button>
-                <button name="depositm" class="btn">Deposit Money</button>
-                <button name="mdonation" class="btn">Make Donation</button>
-                <button name="crcomp" class="btn">Create Complaint Form</button>
-                <button name="mcomment" class="btn">Make Comment</button>
-                <button name="myevents" class="btn">My Events</button>
-                <button name="jgroup" class="btn">Join a Group Tour</button>
-                <button name="janimal" class="btn">Join a Endangered Birthday</button>
-                <button name="logout" class="btn">Logout</button>
-            </form>
-        </div>
-    </div>
-    <script>
-        function toggleuserPopup(){
-            document.getElementById("user-popup").classList.toggle("activate");
-        }
-    </script>
+    <div class="login-popup" id="login-popup">
+                <div class="overlay"></div>
+                <div class="content">
+                	<div class="close" onclick="togglePopup()">×</div>
+					<p class="poptitle">Sign In</p>
+					<div class=tabs>
+						<div class="buttons">
+							<button onclick="show(0)">Visitor</button>
+							<button onclick="show(1)">Employee</button>
+						</div>
+						<div class="loginPanel">
+							<form method = "post">
+								<input id="emaillgn" class="id" type="id" name="emaillgn" placeholder="Email" required="required">
+								<input id="password" class="pass" type="password" name="password" placeholder="Password" required="required">
+								<button class="btn" name="vislogin">Sign In</button>
+							</form>
+						</div>
+						<div class="loginPanel">
+							<form method = "post">
+								<input name="emailemp" class="id" type="id" name="emailemp" placeholder="Email (employee)" required="required">
+								<input name="emppass" class="pass" type="password" name="pass" placeholder="Password" required="required">
+								<button class="btn" name="emploginn">Sign In</button>
+							</form>
+						</div>
+					</div>
+                </div>
+            </div>
+
+            <div class="register-popup" id="register-popup">
+                <div class="overlay"></div>
+                <div class="content">
+                	<div class="close" onclick="toggleRegPopup()">×</div>
+					<p class="poptitle2">Sign Up</p>
+					<form method = "post">
+						<input name="fullname" class="left" type="text" name="name" placeholder="Full Name" required="required">
+						<input name="email" class="right" type="text" name="email" placeholder="Email" required="required">
+						<input name="phoneno" class="left" type="text" name="phone" placeholder="Phone Number" required="required">
+						<input name="dateofbirth" class="right" type="date" name="date" placeholder="Date of Birth" required="required">
+						<input name="password" class="left" type="password" name="pass" placeholder="Password" required="required">
+						<input name="confpass" class="right" type="password" name="pass" placeholder="Confirm Password" required="required">
+						<p class="gender">Gender: </p>
+						<input name="gender" class="radio" type="radio" id="male" value="Male" checked>
+						<label class="lbl" for="male">Male</label>
+						<input class="radio" type="radio" name="gender" id="female" value="Female">
+						<label class="lbl" for="female">Female</label>
+						
+						<p class="gender">Account Type: </p>
+						<input name="type" class="radio" type="radio" id="adult" value="Adult" checked>
+						<label class="lbl" for="adult">Adult</label>
+						<input class="radio" type="radio" name="type" id="student" value="Student">
+						<label class="lbl" for="student">Student</label>
+						<button name="register" class="btn">Sign Up</button>
+					</form>
+                </div>
+            </div>
+
+			<script>
+				var tabbtns = document.querySelectorAll(".tabs .buttons button");
+				var tabPan = document.querySelectorAll(".tabs .loginPanel");
+				function show(index) {
+					tabbtns.forEach(function(node){
+						node.style.backgroundColor="";
+						node.style.color="";
+					});
+					tabbtns[index].style.backgroundColor="#404040";
+					tabbtns[index].style.color="white";
+					tabPan.forEach(function(node){
+						node.style.display="none";
+					});
+					tabPan[index].style.display="block";
+					tabPan[index].style.backgroudColor="#404040";
+				}
+				show(0);
+			</script>
+			<script>
+                function togglePopup(){
+                    document.getElementById("login-popup").classList.toggle("activate");
+                }
+				function toggleRegPopup(){
+                    document.getElementById("register-popup").classList.toggle("activate");
+                }
+				if ( window.history.replaceState ) {
+        			window.history.replaceState( null, null, window.location.href );
+    			}
+            </script>
+
     <!-- CODE HERE -->
         <div style="width:75%; height:75%;  background-color:white; margin-left: 12.5%; margin-top: 20px; border-radius: 20px; margin-bottom:30%;">
                 <h1 class="title"> Comments</h1>
--- src/allcommentsin.php
+++ src/allcommentsin.php
@@ -1,6 +1,80 @@
 <?php
-include("config.php"); 
+include("config.php");
 session_start();
+if($_SESSION['login_user']) 
+{
+    $userid = $_SESSION['login_user']; 
+    $name = $_SESSION['name'];
+	$usertype = $_SESSION['type'];
+    if($_SESSION['type'] == "visitor"){
+		$moneyquery = "select money from visitor where user_id ='". $userid ."'"; 
+        $moneyarr = mysqli_query($mysqli, $moneyquery);
+        $fetchmarr = mysqli_fetch_array($moneyarr, MYSQLI_ASSOC);
+        $money = $fetchmarr['money'];
+        $_SESSION['money'] = $money;
+    }
+} else {
+    header("location: allcomments.php");
+}
+
+if(isset($_POST['logout'])){
+    session_unset(); 
+    session_destroy(); 
+    header("location: allcomments.php");
+    exit;
+}
+
+/*VISITOR*/
+if(isset($_POST['viewp']))
+	header("location: myprofilevisitor.php");
+if(isset($_POST['editp']))
+	header("location: editprofilevisitor.php");
+if(isset($_POST['depositm']))
+	header("location: depositmoney.php");
+if(isset($_POST['mdonation']))
+	header("location: makedonation.php");
+if(isset($_POST['crcomp']))
+	header("location: complaintform.php");
+if(isset($_POST['myevents']))
+	header("location: myevents.php");
+if(isset($_POST['jgroup']))
+	header("location: grouptourin.php");
+if(isset($_POST['janimal']))
+	header("location: birthdayin.php");
+if(isset($_POST['mcomment']))
+	header("location: comment.php");
+
+/* Coordinator */
+if(isset($_POST['viewpemp'])) // common for all emp
+	header("location: myprofileemployee.php");
+if(isset($_POST['editpemp'])) // common for all emp
+	header("location: editprofileemployee.php");
+if(isset($_POST['cagem']))
+	header("location: cagemanagement.php");
+if(isset($_POST['cevent']))
+	header("location: createevents.php");
+if(isset($_POST['rescomp']))
+	header("location: answercomplaint.php");
+if(isset($_POST['regnew']))
+	header("location: createemployee.php");
+
+/*Keeper*/
+if(isset($_POST['mycage']))
+	header("location: mycagesin.php");
+if(isset($_POST['foodm']))
+	header("location: food.php");
+
+/*Guide*/
+if(isset($_POST['mytours']))
+	header("location: guidepage.php");
+
+/*Vet*/
+if(isset($_POST['treatbtn']))
+	header("location: treatment.php"); 
+if(isset($_POST['invbtn']))
+	header("location: myinvitations.php"); 
+if(isset($_POST['treatreq']))
+	header("location: treatmentrequest.php");	
 
 function console_log( $data ){
     echo '<script>';
@@ -16,6 +90,8 @@ function console_log( $data ){
         .title{
             font: 40px helvetica;
             margin-bottom: 10px;
+            border-bottom: 3px solid #d5d5d5;
+            margin-top: 5%;
         }
         .table{
             border-collapse: collapse;
@@ -53,54 +129,145 @@ function console_log( $data ){
 </head>
 
 <body>
-<header>
-<a href="indexin.php" class="header-brand">KasaloZoo</a>
-    <img class="logo" src="image/balina.png" alt="kasalot logo">
-    <nav>
-        <ul>
-            <li><a href="indexin.php">Main Page</a></li>
-            <li><a href="animalsin.php">Animals</a></li>
-            <li><a href="eventsin.php">Events</a></li>
-            <li><a href="aboutin.php">About Zoo</a></li>
-            <?php
-            echo "<li>
-                <a href=\"#\" onclick=\"toggleuserPopup()\">Hello $name ($userid)
-                <img class=\"down\" src=\"image/user.png\" alt=\"user logo\">
-                </a>
-            </li>";
-            if($usertype == "visitor")
-                echo "<li><a href=\"#\">$money
-                <img class=\"down\" src=\"image/dollar.png\" alt=\"dollar logo\">
-                </a></li>";
-            ?>
-        </ul>
-    </nav>
-</header>
+        <header>
+			<a href="indexin.php" class="header-brand">KasaloZoo</a>
+			<img class="logo" src="image/balina.png" alt="kasalot logo">
+			<nav>
+				<ul>
+                    <li><a href="indexin.php">Main Page</a></li>
+					<li><a href="animalsin.php">Animals</a></li>
+					<li><a href="eventsin.php">Events</a></li>
+					<li><a href="aboutin.php">About Zoo</a></li>
+                    <?php
+                    echo "<li>
+                        <a href=\"#\" onclick=\"toggleuserPopup()\">Hello $name ($userid)
+                        <img class=\"down\" src=\"image/user.png\" alt=\"user logo\">
+                        </a>
+                    </li>";
+                    if($usertype == "visitor")
+						echo "<li><a href=\"#\">$money
+						<img class=\"down\" src=\"image/dollar.png\" alt=\"dollar logo\">
+						</a></li>";
+                    ?>
+				</ul>
+			</nav>
+		</header>
 <main>
-<div class="user-popup" id="user-popup">
-        <div class="overlay"></div>
-        <div class="content">
-            <div class="close" onclick="toggleuserPopup()">×</div>
-            <h2 class="h2pop">Operations</h2>
-            <form method = "post">
-                <button name="viewp" class="btn">View Profile</a></button>
-                <button name="editp" class="btn">Edit Profile</button>
-                <button name="depositm" class="btn">Deposit Money</button>
-                <button name="mdonation" class="btn">Make Donation</button>
-                <button name="crcomp" class="btn">Create Complaint Form</button>
-                <button name="mcomment" class="btn">Make Comment</button>
-                <button name="myevents" class="btn">My Events</button>
-                <button name="jgroup" class="btn">Join a Group Tour</button>
-                <button name="janimal" class="btn">Join a Endangered Birthday</button>
-                <button name="logout" class="btn">Logout</button>
-            </form>
-        </div>
-    </div>
-    <script>
-        function toggleuserPopup(){
-            document.getElementById("user-popup").classList.toggle("activate");
-        }
-    </script>
+            <div class="user-popup" id="user-popup">
+                <div class="overlay"></div>
+                <div class="content">
+                	<div class="close" onclick="toggleuserPopup()">×</div>
+                    <h2 class="h2pop">Operations</h2>
+                    <form method = "post">
+                        <button name="viewp" class="btn">View Profile</a></button>
+                        <button name="editp" class="btn">Edit Profile</button>
+                        <button name="depositm" class="btn">Deposit Money</button>
+                        <button name="mdonation" class="btn">Make Donation</button>
+                        <button name="crcomp" class="btn">Create Complaint Form</button>
+						<button name="mcomment" class="btn">Make Comment</button>
+                        <button name="myevents" class="btn">My Events</button>
+                        <button name="jgroup" class="btn">Join a Group Tour</button>
+                        <button name="janimal" class="btn">Join a Endangered Birthday</button>
+                        <button name="logout" class="btn">Logout</button>
+                    </form>
+                </div>
+            </div>
+
+            <div class="user-popup" id="coor-popup">
+                <div class="overlay"></div>
+                <div class="content">
+                	<div class="close" onclick="toggleuserPopup()">×</div>
+                    <h2 class="h2pop">Operations</h2>
+                    <form method = "post">
+                        <button name="viewpemp" class="btn">View Profile</button>
+                        <button name="editpemp" class="btn">Edit Profile</button>
+                        <button name="cagem" class="btn">Cage Management</button>
+                        <button name="cevent" class="btn">Create Event</button>
+                        <button name="rescomp" class="btn">Respond to Complaint Forms</button>
+                        <button name="regnew" class="btn">Register a New Employee</button>
+                        <button name="logout" class="btn">Logout</button>
+                    </form>
+                </div>
+            </div>
+
+			<div class="user-popup" id="keep-popup">
+                <div class="overlay"></div>
+                <div class="content">
+                	<div class="close" onclick="toggleuserPopup()">×</div>
+                    <h2 class="h2pop">Operations</h2>
+                    <form method = "post">
+                        <button name="viewpemp" class="btn">View Profile</button>
+                        <button name="editpemp" class="btn">Edit Profile</button>
+                        <button name="mycage" class="btn">My Cages</button>
+						<button name="foodm" class="btn">Food Stock Management</button>
+                        <button name="logout" class="btn">Logout</button>
+                    </form>
+                </div>
+            </div>
+
+			<div class="user-popup" id="guide-popup">
+                <div class="overlay"></div>
+                <div class="content">
+                	<div class="close" onclick="toggleuserPopup()">×</div>
+                    <h2 class="h2pop">Operations</h2>
+                    <form method = "post">
+                        <button name="viewpemp" class="btn">View Profile</button>
+                        <button name="editpemp" class="btn">Edit Profile</button>
+                        <button name="mytours" class="btn">My Tours</button>
+                        <button name="logout" class="btn">Logout</button>
+                    </form>
+                </div>
+            </div>
+
+			<div class="user-popup" id="vet-popup">
+                <div class="overlay"></div>
+                <div class="content">
+                	<div class="close" onclick="toggleuserPopup()">×</div>
+                    <h2 class="h2pop">Operations</h2>
+                    <form method = "post">
+                        <button name="viewpemp" class="btn">View Profile</button>
+                        <button name="editpemp" class="btn">Edit Profile</button>
+                        <button name="treatbtn" class="btn">Treatments</button>
+						<button name="treatreq" class="btn">Treatment Requests</button>
+						<button name="invbtn" class="btn">Invitations</button>
+                        <button name="logout" class="btn">Logout</button>
+                    </form>
+                </div>
+            </div>
+
+			<?php
+				if($usertype == "visitor"){
+					echo "<script>
+						function toggleuserPopup(){
+							document.getElementById(\"user-popup\").classList.toggle(\"activate\");
+						}
+					</script>";
+				} else if($usertype == "keeper") {
+					echo "<script>
+					function toggleuserPopup(){
+						document.getElementById(\"keep-popup\").classList.toggle(\"activate\");
+					}
+					</script>";
+				} else if($usertype == "vet") {
+					echo "<script>
+					function toggleuserPopup(){
+						document.getElementById(\"vet-popup\").classList.toggle(\"activate\");
+					}
+					</script>";
+				} else if($usertype == "coor") {
+					echo "<script>
+					function toggleuserPopup(){
+						document.getElementById(\"coor-popup\").classList.toggle(\"activate\");
+					}
+					</script>";
+				} else if($usertype == "guide") {
+					echo "<script>
+					function toggleuserPopup(){
+						document.getElementById(\"guide-popup\").classList.toggle(\"activate\");
+					}
+					</script>";
+				}
+			?>
     <!-- CODE HERE -->
         <div style="width:75%; height:75%;  background-color:white; margin-left: 12.5%; margin-top: 20px; border-radius: 20px; margin-bottom:30%;">
                 <h1 class="title"> Comments</h1>
